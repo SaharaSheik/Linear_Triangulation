05fda5baf05f4586b515ee16695cb825e310e68f
Sahara Sheik <84471457+SaharaSheik@users.noreply.github.com>
2024-02-23 14:14:03 -0500

    examples of Newkuba was added
    examples of Newkuba was added

---
+++ examples/Newkuba/calib.m
@@ -0,0 +1,12 @@
+im1=imread('im1.png');
+im2=imread('im2.png');
+cam1=[998.834 0 327.302; 0 998.834 245.534; 0 0 1];
+cam2=[998.834 0 368.275; 0 998.834 245.534; 0 0 1];
+doffs=40.973;
+baseline=203.047;
+width=701;
+height=487;
+ndisp=143;
+
+
+[pntcloud, worldPoints, CloudSNR] = linbackproj(im1,im2,cam1,cam2,baseline,width,height);
