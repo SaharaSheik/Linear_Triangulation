0ea44ec5fcc6af196e5189983962da7addcd91be
Sahara Sheik <84471457+SaharaSheik@users.noreply.github.com>
2024-02-23 14:12:50 -0500

    examples of globe added
    examples of globe

---
+++ examples/globe/calib.m
@@ -0,0 +1,11 @@
+im1=imread('im1.png');
+im2=imread('im2.png');
+cam1=[1037.6 0 642.2316; 0 1043.3 387.8358; 0 0 1];
+cam2=[998.834 0 368.275; 0 998.834 245.534; 0 0 1];
+doffs=0;
+baseline=200;
+width=1280;
+height=720;
+
+
+[pntcloud, worldPoints, CloudSNR] = linbackproj(im1,im2,cam1,cam2,baseline,width,height);
